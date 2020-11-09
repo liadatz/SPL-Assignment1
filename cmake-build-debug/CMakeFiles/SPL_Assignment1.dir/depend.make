6ae3cfe636e25c40a95ecffb433753b0474fa1fd
liad <liad.atsmon@gmail.com>
2020-11-09 16:25:06 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/SPL_Assignment1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SPL_Assignment1.
+# This may be replaced when dependencies are built.
--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ int main(int argc, char** argv){
     if(argc != 2){
         cout << "usage cTrace <config_path>" << endl;
         return 0;
-        int a = 0;
     }
     Session sess(argv[1]);
     sess.simulate();
